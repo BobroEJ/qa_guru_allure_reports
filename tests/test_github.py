9793130ae22e3e9a0730816a153fd7d3e9a67129
bobroej <tverdune@yandex.ru>
2022-07-19 09:19:40 +0300

    all variants are done

--- tests/test_github.py
+++ tests/test_github.py
@@ -12,12 +12,25 @@ def browser_init():
     browser.config._hold_browser_open = True
 
 
-def test_github():
+def test_selene():
+
+    browser.open('https://github.com/')
+
+    browser.element('.header-search-input').type('eroshenkoam/allure-example').press_enter()
+
+    browser.element(by.link_text("eroshenkoam/allure-example")).click()
+
+    browser.element('#issues-tab').click()
+
+    browser.element(by.partial_text('#76')).should(be.visible)
+
+
+def test_dynamic_steps():
 
     with allure.step('Открываем главную страницу'):
         browser.open('https://github.com/')
 
-    with allure.step('Ищем репоситорий'):
+    with allure.step('Ищем репозиторий'):
         browser.element('.header-search-input').type('eroshenkoam/allure-example').press_enter()
 
     with allure.step('Переходим по ссылке репозитория'):
@@ -28,3 +41,36 @@ def test_github():
 
     with allure.step(f'Проверяем наличие Issue с номером 76'):
         browser.element(by.partial_text('#76')).should(be.visible)
+
+
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository('eroshenkoam/allure-example')
+    go_to_repository('eroshenkoam/allure-example')
+    open_issue_tab()
+    should_see_issue_with_number(76)
+
+
+@allure.step('Открываем главную страницу')
+def open_main_page():
+    browser.open('https://github.com/')
+
+
+@allure.step('Ищем репозиторий')
+def search_for_repository(repo):
+    browser.element('.header-search-input').type(repo).press_enter()
+
+
+@allure.step('Переходим по ссылке репозитория')
+def go_to_repository(repo):
+    browser.element(by.link_text(repo)).click()
+
+
+@allure.step('Открываем таб Issues')
+def open_issue_tab():
+    browser.element('#issues-tab').click()
+
+
+@allure.step('Проверяем наличие Issue с номером {number}')
+def should_see_issue_with_number(number):
+    browser.element(by.partial_text(f'#{number}')).should(be.visible)
